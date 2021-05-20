1e79b4f817a01f911a6dbb2dd6bfd02cd5fb9f10
Maciel Sousa <fmacielsousa@gmail.com>
2021-05-20 15:10:44 -0300

    Manipulando Arrays com filter e map

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-//Exemplo 1
-//Como sonsumir dados de uma API utilizando o fetch
-fetch('http://localhost:3003/produtos').then((response) =>
- response.json()
- ).then(json => {console.log('Exemplo 1 ',json)});
+const precos = [
+    'Crédito',
+    'R$ 200',
+    'R$ 400',
+    'Contas Pagar',
+    'R$ 300',
+    'Meus dados',
+]
 
- //Exemplo 2
- //Como consumir dados de uma API utilizando o fetch
- async function fetchProdutos(url){
-     const response = await fetch(url);
-     const json = await response.json();
-     return json;
- }
-
- const produtos = fetchProdutos('http://localhost:3003/produtos');
- console.log('Exemplo 2 ',produtos);
+const precosFiltros = precos.filter((preco) => preco.includes('R$'))
+const precosNumeros = precosFiltros.map( (preco) => Number(preco.replace('R$ ','')))
+console.log(precosNumeros)
